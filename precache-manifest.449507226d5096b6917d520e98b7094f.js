970c6f4c51d20e79053b09eb2390d89a76c894e9
Davina Leong <davina.leong@outlook.com>
2020-01-17 15:16:14 +0800

    Added missing files

---
+++ precache-manifest.449507226d5096b6917d520e98b7094f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e9f5038697ac1a78c7e2c8c8663f54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edf26f12e3b6a3b7af14",
+    "url": "/static/css/main.b54f960e.chunk.css"
+  },
+  {
+    "revision": "02b20a6a6f2a63179df8",
+    "url": "/static/js/2.56e8dd6b.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/static/js/2.56e8dd6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edf26f12e3b6a3b7af14",
+    "url": "/static/js/main.1d7ff86f.chunk.js"
+  },
+  {
+    "revision": "913854436fbcbf917fdb",
+    "url": "/static/js/runtime-main.886ae9b2.js"
+  }
+]);
