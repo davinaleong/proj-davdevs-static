e2032e6cde56063a41b3f48bba3e0d8684e87ee9
Davina Leong <davina.leong@outlook.com>
2020-01-17 15:24:44 +0800

    Removed duplicated project

---
+++ precache-manifest.c69bef494601de9e8610dc86fcab2721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb6ef0120c7ab59250a10d2f3556b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd24aaeb3fbc7d42c7ba",
+    "url": "/static/css/main.b54f960e.chunk.css"
+  },
+  {
+    "revision": "02b20a6a6f2a63179df8",
+    "url": "/static/js/2.56e8dd6b.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/static/js/2.56e8dd6b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd24aaeb3fbc7d42c7ba",
+    "url": "/static/js/main.e9d0868e.chunk.js"
+  },
+  {
+    "revision": "913854436fbcbf917fdb",
+    "url": "/static/js/runtime-main.886ae9b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.449507226d5096b6917d520e98b7094f.js"
+  "/precache-manifest.c69bef494601de9e8610dc86fcab2721.js"
 );
 
 self.addEventListener('message', (event) => {
